702896f9064aae7563ccbf621512560b2000d5da
Niklaus Hirt <niklaushirt@gmail.com>
2022-06-29 16:51:09 +0200

    20220629-1651

--- 01_easy-install.sh
+++ 01_easy-install.sh
@@ -21,7 +21,7 @@ export WAIOPS_VERSION=34
 
 export SHOW_MORE="false"
 export WAIOPS_PODS_MIN=115
-export DOC_URL="https://github.ibm.com/NIKH/aiops-install-ansible-33#2-ai-manager-installation"
+export DOC_URL="https://github.com/niklaushirt/cp4waiops-deployer#2-ai-manager-installation"
 
 
 # ---------------------------------------------------------------------------------------------------------------"
