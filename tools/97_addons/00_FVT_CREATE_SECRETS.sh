e524cb6ca9285d679aa09b97f2f30a64a941c651
Niklaus Hirt <niklaushirt@gmail.com>
2022-09-13 18:09:19 +0200

    20220913-1809

--- tools/97_addons/00_FVT_CREATE_SECRETS.sh
+++ tools/97_addons/00_FVT_CREATE_SECRETS.sh
@@ -8,12 +8,8 @@ echo ""
 echo "------------------------------------------------------------------------------------------------------------------------------"
 echo " 🧻 Delete existing Secrets"
 oc delete secret ibm-aiops-pull-secret -n cp4waiops
-oc delete secret ibm-aiops-pull-secret -n cp4waiops
-oc delete secret ibm-aiops-pull-secret -n cp4waiops-evtmgr
 oc delete secret ibm-aiops-pull-secret -n cp4waiops-evtmgr
 oc delete secret ibm-aiops-pull-secret -n openshift-marketplace
-oc delete secret ibm-aiops-pull-secret -n openshift-marketplace
-oc delete secret ibm-aiops-pull-secret -n openshift-operators
 oc delete secret ibm-aiops-pull-secret -n openshift-operators
 echo ""
 echo ""
@@ -31,8 +27,6 @@ echo ""
 
 echo "------------------------------------------------------------------------------------------------------------------------------"
 echo " 📥 Set Logins"
-export DOCKER_USER=your_docker_username
-export DOCKER_PWD=your_docker_password
 
 # The pull token for cp.icr.com from https://myibm.ibm.com/products-services/containerlibrary
 export ICR_TOKEN=your_IBM_PULL_TOKEN
@@ -48,7 +42,6 @@ echo "--------------------------------------------------------------------------
 echo " 🔐 Create Pull Secret File"
 oc get secret/pull-secret -n openshift-config -oyaml > pull-secret-backup.yaml
 oc get secret/pull-secret -n openshift-config --template='{{index .data ".dockerconfigjson" | base64decode}}' > temp-pull-secret.yaml
-oc registry login --registry="docker.io" --auth-basic="$DOCKER_USER:$DOCKER_PWD" --to=temp-pull-secret.yaml
 oc registry login --registry="hyc-katamari-cicd-team-docker-local.artifactory.swg-devops.com" --auth-basic="$ARTIFACTORY_USER:$ARTIFACTORY_TOKEN" --to=temp-pull-secret.yaml
 oc registry login --registry="cp.icr.io" --auth-basic="cp:$ICR_TOKEN" --to=temp-pull-secret.yaml
 oc registry login --registry="cp.stg.icr.io" --auth-basic="cp:$ICR_TOKEN" --to=temp-pull-secret.yaml
@@ -62,11 +55,6 @@ echo ""
 
 echo "------------------------------------------------------------------------------------------------------------------------------"
 echo " 🚀 Creating Pull Secrets"
-oc create secret generic ibm-aiops-pull-secret -n cp4waiops --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=temp-ibm-aiops-pull-secret.yaml
-oc create secret generic ibm-aiops-pull-secret -n cp4waiops-evtmgr --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=temp-ibm-aiops-pull-secret.yaml
-oc create secret generic ibm-aiops-pull-secret -n openshift-marketplace --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=temp-ibm-aiops-pull-secret.yaml
-oc create secret generic ibm-aiops-pull-secret -n openshift-operators --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=temp-ibm-aiops-pull-secret.yaml
-
 oc create secret generic ibm-aiops-pull-secret -n cp4waiops --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=temp-ibm-aiops-pull-secret.yaml
 oc create secret generic ibm-aiops-pull-secret -n cp4waiops-evtmgr --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=temp-ibm-aiops-pull-secret.yaml
 oc create secret generic ibm-aiops-pull-secret -n openshift-marketplace --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=temp-ibm-aiops-pull-secret.yaml
